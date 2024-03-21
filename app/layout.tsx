a5f55cb6986e33d8d4439448e0c91a8dbf6cf3eb
Francisco Guerra <paco.gq.0709@gmail.com>
2024-03-21 14:09:43 -0600

    image when sharing test 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from 'next/head';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Cassad",
-  description: "Cassad",
+  description: "Servicios inmobiliarios y legales",
 };
 
 export default function RootLayout({
@@ -16,6 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:title" content="Cassad" />
+        <meta property="og:description" content="Servicios inmobiliarios y legales" />
+        <meta property="og:image" content="/logos/logo_fondo_blanco.jpg" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );

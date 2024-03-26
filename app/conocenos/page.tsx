3cbeadcb237e4af0749ad0f7fd21e0cfc7aeeb9c
Francisco Guerra <paco.gq.0709@gmail.com>
2024-03-25 19:07:19 -0600

    text change

--- app/conocenos/page.tsx
+++ app/conocenos/page.tsx
@@ -82,7 +82,7 @@ export default function Conocenos() {
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="mt-1 h-5 w-5 flex-none text-mainAzul-100">
                                             <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
                                         </svg>
-                                        <span><strong className="font-semibold text-gray-900">Experiencia en Administración.</strong> Con más de una década de experiencia en el sector, contamos con un equipo altamente capacitado y con un profundo conocimiento del mercado, garantizando una gestión eficiente y efectiva de propiedades.</span>
+                                        <span><strong className="font-semibold text-gray-900">Experiencia en Administración.</strong> Se cuenta con el conocimiento legal y mecanismos necesarios para poder llevar una administración eficaz y eficiente.</span>
                                     </li>
 
                                     <li className="flex gap-x-3">
